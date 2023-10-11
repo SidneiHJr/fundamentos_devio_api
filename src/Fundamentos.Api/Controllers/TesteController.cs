590e0223dce1a3d80e3841e01b5b286575b7f84d
Sidnei Hernandes Junior <sidnei.hernandes@treeinova.com.br>
2023-10-10 21:58:38 -0300

    ate documentacao com swagger

---
+++ src/Fundamentos.Api/Controllers/TesteController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fundamentos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/demo")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Produto { Id = 1, Nome = "Produto Teste" });
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Produto { Id = id, Nome = "Produto Teste" });
+        }
+
+        [HttpPost]
+        public IActionResult Post(Produto produto)
+        {
+            return CreatedAtAction("Get", new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.Id) return BadRequest();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ src/Fundamentos.Api/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace Fundamentos.Api
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+    }
+}
